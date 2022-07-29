7c4b0dd52b53246ba3022ef32b76f2894c16d00e
OrionMaximov <quentin.nicodeme@gmail.com>
2022-07-29 10:40:55 +0200

    defaut avec les imports et je trouve pas la soluce

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-import  {zoom}  from "./lib/zoom";
+/* import  {zoom}  from "./lib/zoom";
 import {show} from "./lib/scroll.js";
-import {scrollToTop} from "./lib/scrollTop";
+import {scrollToTop} from "./lib/scrollTop"; */
 
 /* ANIMATION SLIDER */
 const sliderImg = document.querySelector("#slider>img");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 
-const urlIlmg = [
+const urlImg = [
     "assets/img/mac.jpg",
     "assets/img/macbook-air-m2-2022-point-m1.jpeg",
     "assets/img/604682.jpeg",
@@ -16,21 +16,21 @@ const urlIlmg = [
 
 // 1ère étape je déclare un index
 let i = 0;
-sliderImg.src = urlIlmg[i];
+sliderImg.src = urlImg[i];
 next.addEventListener(
     "click",
     function () {
         console.log(i);
         // 2ème étape: limité  index à la taille de mon tableau -1
-        if (i === urlIlmg.length-1) {
+        if (i === urlImg.length-1) {
             i = 0;
-            sliderImg.src = urlIlmg[i];
+            sliderImg.src = urlImg[i];
         } else {
             // 3ème étape : j'incrémente mon index
         //i = i + 1;
         i++; // incrémentation
             // je dois réaffecter src avec la nouvelle valeur de index
-            sliderImg.src = urlIlmg[i];
+            sliderImg.src = urlImg[i];
         }
     }
 )
@@ -39,11 +39,11 @@ prev.addEventListener(
     function () {
         console.log(i);
         if (i === 0){
-            i = urlIlmg.length-1; 
-            sliderImg.src = urlIlmg[i];
+            i = urlImg.length-1; 
+            sliderImg.src = urlImg[i];
         } else {
         i--;
-        sliderImg.src = urlIlmg[i];
+        sliderImg.src = urlImg[i];
         }
     }
 )
@@ -52,16 +52,16 @@ setInterval(
         console.log(i);
         if (i === urlIlmg.length-1) {
             i = 0;
-            sliderImg.src = urlIlmg[i];
+            sliderImg.src = urlImg[i];
         } else {
         i++; 
-            sliderImg.src = urlIlmg[i];
+            sliderImg.src = urlImg[i];
         }
     },
     3000
 )
 
-scrollToTop();
+/* scrollToTop();
 show();
 zoom();
-
+ */
